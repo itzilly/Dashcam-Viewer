caf28ca14c9ea9dbe853d9d9a11e905379556302
illyum <90023277+itzilly@users.noreply.github.com>
2023-07-09 22:34:56 -0600

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+
+namespace Dashcam_Viewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FileMenu_OpenSdCard_Click(object sender, RoutedEventArgs e)
+        {
+            // string currentDirectory = "C:\\";
+            // var dlg = new CommonOpenFileDialog();
+            // dlg.Title = "Select Drive";
+            // dlg.IsFolderPicker = true;
+            // dlg.InitialDirectory = currentDirectory;
+
+            // dlg.AddToMostRecentlyUsedList = false;
+            // dlg.AllowNonFileSystemItems = false;
+            // dlg.DefaultDirectory = currentDirectory;
+            // dlg.EnsureFileExists = true;
+            // dlg.EnsurePathExists = true;
+            // dlg.EnsureReadOnly = false;
+            // dlg.EnsureValidNames = true;
+            // dlg.Multiselect = false;
+            //dlg.ShowPlacesList = true;
+
+            // if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+            // {
+            //     var folder = dlg.FileName;
+            //     Debug.Print(folder);
+            // }
+
+            SelectDriveWindow driveWindow = new SelectDriveWindow();
+            driveWindow.Show();
+
+            string selectedDrive = driveWindow.SelectedDrivePath;
+            if (selectedDrive != null)
+            {
+
+            }
+        }
+
+        void FileMenu_SelectClipsFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new CommonOpenFileDialog();
+            dlg.ShowDialog();
+        }
+
+        private void FileMenu_Settings_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FileMenu_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ SelectDriveWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Dashcam_Viewer
+{
+    /// <summary>
+    /// Interaction logic for SelectDriveWindow.xaml
+    /// </summary>
+    public partial class SelectDriveWindow : Window
+    {
+        public string SelectedDrivePath {  get; set; }
+
+        private DispatcherTimer refreshTimer;
+
+        public SelectDriveWindow()
+        {
+            InitializeComponent();
+            refreshTimer = new DispatcherTimer();
+            refreshTimer.Interval = TimeSpan.FromSeconds(2);
+            refreshTimer.Tick += UpdateDrivesList;
+            refreshTimer.Start();
+        }
+
+        private void UpdateDrivesList(object sender, EventArgs e)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            Debug.Print("Located Drives:");
+            var removeableDrives = new List<DriveInfo>();
+            foreach (DriveInfo drive in allDrives)
+            {
+                if (drive.DriveType == DriveType.Removable)
+                {
+                    removeableDrives.Add(drive);
+                }
+            }
+        }
+
+        private void Select_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedDrivePath = string.Empty;
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
